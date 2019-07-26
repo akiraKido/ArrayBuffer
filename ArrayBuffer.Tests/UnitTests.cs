77873ceddcfa93682a06a88a7a42930026ee1794
Akira Kido <achts.y@gmail.com>
2019-07-26 17:19:12 +0900

    (should've been) initial commit

---
+++ ArrayBuffer.Tests/UnitTests.cs
@@ -0,0 +1,129 @@
+using Xunit;
+using FluentAssertions;
+
+// ReSharper disable once CheckNamespace
+namespace ArrayBufferNS.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void TestSmallArray()
+        {
+            var buffer = new ArrayBuffer<int>(10);
+
+            var span = buffer.Take(10);
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i] = i;
+            }
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i].Should().Be(i);
+            }
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                buffer.Array[i].Should().Be(i);
+            }
+        }
+
+        [Fact]
+        public void TestArrayOverInitialSize()
+        {
+            var buffer = new ArrayBuffer<int>(10);
+
+            var span = buffer.Take(20);
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i] = i;
+            }
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i].Should().Be(i);
+            }
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                buffer.Array[i].Should().Be(i);
+            }
+        }
+
+        [Fact]
+        public void TestMoreThan1Take()
+        {
+            var buffer = new ArrayBuffer<int>(10);
+
+            var span1 = buffer.Take(10);
+            var span2 = buffer.Take(10);
+
+            for (int i = 0; i < span2.Length; i++)
+            {
+                span2[i] = i;
+            }
+
+            for (int i = 0; i < span1.Length; i++)
+            {
+                span1[i].Should().Be(0);
+            }
+
+            for (int i = 0; i < span2.Length; i++)
+            {
+                span2[i].Should().Be(i);
+            }
+        }
+
+        [Fact]
+        public void TestReuse()
+        {
+            var buffer = new ArrayBuffer<int>(10);
+            
+            var span = buffer.Take(10);
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i] = (i + 1) * 10;
+            }
+
+            span.Return();
+            span = buffer.Take(10);
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i].Should().Be(0);
+            }
+
+            buffer.Array.Count.Should().Be(10);
+        }
+
+        [Fact]
+        public void TestReuseAndCheckMiddle()
+        {
+            var buffer = new ArrayBuffer<int>(10);
+
+            var span1 = buffer.Take(10);
+            var span2 = buffer.Take(10);
+            var span3 = buffer.Take(10);
+
+            buffer.Array.Count.Should().Be(40);
+            
+            for (int i = 0; i < span2.Length; i++)
+            {
+                span2[i] = i;
+            }
+            
+            span2.Return();
+
+            for (int i = 0; i < 10; i++)
+            {
+                span1[i].Should().Be(0);
+                span2[i].Should().Be(0);
+                span3[i].Should().Be(0);
+            }
+
+            for (int i = 10; i < 20; i++)
+            {
+                buffer.Array[i].Should().Be(0);
+            }
+        }
+    }
+}
---
+++ ArrayBuffer/ArrayBuffer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using BinaryDictionaryNS;
+
+// ReSharper disable once CheckNamespace
+namespace ArrayBufferNS
+{
+    public class ArrayBuffer<T>
+    {
+        public struct Span : IDisposable
+        {
+            public readonly int Length;
+
+            private readonly int start;
+            private readonly ArrayBuffer<T> parent;
+
+            public Span(ArrayBuffer<T> parent, int start, int length)
+            {
+                this.start = start;
+                Length = length;
+
+                this.parent = parent;
+            }
+
+            public T this[int i]
+            {
+                get => parent.array[start + i];
+                set => parent.array[start + i] = value;
+            }
+
+            public void Return()
+            {
+                for (int i = start; i < start + Length; i++)
+                {
+                    parent.array[i] = default;
+                    parent.usedIndexes.Remove(i);
+                }
+            }
+
+            public void Dispose()
+            {
+                Return();
+            }
+        }
+
+        public IReadOnlyList<T> Array => array;
+
+        private T[] array;
+        private readonly BinaryDictionary usedIndexes = new BinaryDictionary();
+
+        public ArrayBuffer() : this(10) { }
+
+        public ArrayBuffer(int size)
+        {
+            array = new T[size];
+        }
+
+        public Span Take(int size)
+        {
+            CheckArraySize(size);
+
+            int start = -1;
+
+            while (true)
+            {
+                for (int i = start + 1; i < array.Length; i++)
+                {
+                    if (usedIndexes.Contains(i))
+                    {
+                        continue;
+                    }
+
+                    start = i;
+                    break;
+                }
+
+                bool isAvailable = true;
+                for (int i = start; i < start + size; i++)
+                {
+                    if (usedIndexes.Contains(i) == false)
+                    {
+                        continue;
+                    }
+
+                    isAvailable = false;
+                    break;
+                }
+
+                if (isAvailable)
+                {
+                    break;
+                }
+            }
+
+            for (int i = start; i < start + size; i++)
+            {
+                usedIndexes.Add(i);
+            }
+
+            return new Span(this, start, size);
+        }
+
+        private void CheckArraySize(int size)
+        {
+            if (size <= array.Length - usedIndexes.Count())
+            {
+                return;
+            }
+
+            var newSize = array.Length * 2;
+            if (newSize < size)
+            {
+                newSize = size * 2;
+            }
+
+            var newArray = new T[newSize];
+            System.Array.Copy(array, 0, newArray, 0, array.Length);
+            array = newArray;
+        }
+    }
+}
